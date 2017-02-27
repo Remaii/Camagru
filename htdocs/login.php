c31728fe7368b8c6c6efc06ee94eab632a75b0ba
Remaii <remy.thidet@hotmail.fr>
2017-02-27 03:07:55 +0100

    maj page de log js

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,21 +1,10 @@
-<style type="text/css">
-	#form_log
-	{
-		position: absolute;
-		left: 45%;
-		top: 40%;
-		text-align: left;
-		width: 20vw;
-	}
-</style>
-
+<input id="changeur" type="button" value="Creer un compte"><br>
 <form id="form_log" method="post" action="function/connect.php">
-	<input id="changeur" type="button" value="Creer un compte"><br>
 	<label style="color:white;">Login</label><br>
 	<input type="text" name="login"><br>
 	<label style="color:white;">Mot de passe</label><br>
-	<input type="password" name="pwd">
-	<input id="forgot" type="button" href="function/select.php?to=resetpwd" value="Oublier?"><br>
-	<input type="submit" name="log_but" value="Login">
+	<input type="password" name="pwd"><br>
+	<input id="sub" type="submit" name="log_but" value="Login">
+	<input id="forgot" type="button" href="function/select.php?to=resetpwd" value="Oublier?">
 </form>
 <script src="js/login.js"></script>
---
+++ js/login.js
@@ -0,0 +1,101 @@
+function toLogin() {
+	var form = document.getElementById('form_log'),
+		forgot = document.createElement('input'),
+		lab_log = document.createElement('label'),
+		login = document.createElement('input'),
+		lab_pwd = document.createElement('label'),
+		pwd = document.createElement('input'),
+		submit = document.createElement('input');
+
+	form.innerHTML = '';
+	form.setAttribute('id','form_log');
+	form.setAttribute('method','post');
+	form.setAttribute('action','function/connect.php');
+	forgot.setAttribute('href','function/select.php?to=resetpwd');
+	forgot.setAttribute('type','button');
+	forgot.setAttribute('value','Oublier?');
+	lab_log.innerHTML = "Login<br>";
+	login.setAttribute('type','text');
+	login.setAttribute('name','login');
+	lab_pwd.innerHTML = "Mot de Passe<br>";
+	pwd.setAttribute('type','password');
+	pwd.setAttribute('name','pwd');
+	submit.setAttribute('id', 'sub');
+	submit.setAttribute('type', 'submit');
+	submit.setAttribute('value', 'Login');
+	submit.setAttribute('name', 'log_but');
+
+	form.appendChild(lab_log);
+	form.appendChild(login);
+	form.innerHTML += '<br>';
+	form.appendChild(lab_pwd);
+	form.appendChild(pwd);
+	form.innerHTML += '<br>';
+	form.appendChild(submit);
+	form.appendChild(forgot);
+}
+function toRegister() {
+	var form = document.getElementById('form_log'),
+		lab_log = document.createElement('label'),
+		login = document.createElement('input'),
+		lab_pwd = document.createElement('label'),
+		pwd = document.createElement('input'),
+		lab_cfpwd = document.createElement('label'),
+		cfpwd = document.createElement('input'),
+		lab_mail = document.createElement('label'),
+		mail = document.createElement('input'),
+		submit = document.createElement('input');
+
+	form.innerHTML = '';
+	form.setAttribute('id','form_log');
+	form.setAttribute('method','post');
+	form.setAttribute('action','function/create_account.php');
+	lab_log.innerHTML = "Login<br>";
+	login.setAttribute('type','text');
+	login.setAttribute('name','login');
+	lab_pwd.innerHTML = "Mot de Passe<br>";
+	pwd.setAttribute('type','password');
+	pwd.setAttribute('name','pwd');
+	lab_cfpwd.innerHTML = "Confirmer Mot de Passe<br>";
+	cfpwd.setAttribute('type','password');
+	cfpwd.setAttribute('name','cfpwd');
+	lab_mail.innerHTML = "Mail<br>";
+	mail.setAttribute('type','mail');
+	mail.setAttribute('name','mail');
+	submit.setAttribute('id', 'sub');
+	submit.setAttribute('type', 'submit');
+	submit.setAttribute('value', 'Register');
+	submit.setAttribute('name', 'regist_but');
+
+	form.appendChild(lab_log);
+	form.appendChild(login);
+	form.innerHTML += '<br>';
+	form.appendChild(lab_pwd);
+	form.appendChild(pwd);
+	form.innerHTML += '<br>';
+	form.appendChild(lab_cfpwd);
+	form.appendChild(cfpwd);
+	form.innerHTML += '<br>';
+	form.appendChild(lab_mail);
+	form.appendChild(mail);
+	form.innerHTML += '<br>';
+	form.appendChild(submit);
+}
+
+
+var change = document.getElementById('changeur');
+
+change.addEventListener('click', function(){
+	var submit = document.getElementById('sub'),
+		name = submit.getAttribute('name');
+
+	if (name == 'log_but'){
+		toRegister();
+		change.setAttribute('value', 'Connection');
+	} else if (name == 'regist_but') {
+		toLogin();
+		change.setAttribute('value', 'Créer un compte');
+	} else {
+		alert('EasterEgg!');
+	}
+}, false);
